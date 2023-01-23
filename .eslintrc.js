924dc52142a50f031b39300b70384a6f493d56d2
dariaddz <dariadz@gmail.com>
2023-01-23 11:05:54 +0300

    eslint master

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable object-curly-spacing */
+/* eslint-disable quotes */
+/* eslint-disable linebreak-style */
 module.exports = {
-  'env': {
-    'commonjs': true,
-    'es2021': true,
-    'node': true,
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
   },
-  'extends': 'google',
-  'overrides': [
-  ],
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-  },
-  'rules': {
-  },
+  extends: "google",
+  overrides: [],
+  parserOptions: { ecmaVersion: "latest" },
+  rules: {},
 };
